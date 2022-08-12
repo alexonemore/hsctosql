009db8d923b6a8f4e2606086b793988a5cf5367c
Alex <alexonemoreemail@gmail.com>
2022-08-12 10:55:21 +0300

    backup

--- database.cpp
+++ database.cpp
@@ -1,3 +1,21 @@
+/* This file is part of HSCToSQL (Database converter).
+ * Copyright (c) 2022 Alexandr Shchukin
+ * Corresponding email: shchukin@ism.ac.ru
+ *
+ * HSCToSQL (Database converter) is free software:
+ * you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * HSCToSQL (Database converter) is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HSCToSQL (Database converter).
+ * If not, see <http://www.gnu.org/licenses/>.*/
+
 #include "database.h"
 #include <QFile>
 #include <QDebug>
